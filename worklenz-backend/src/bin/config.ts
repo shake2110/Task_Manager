2c587cf8e498530eb1e17a996be3bda10625746b
shakee <abhishake2110@gmail.com>
2026-05-01 01:14:47 +0530

    Remove segfault-handler to fix Railway build failure

--- worklenz-backend/src/bin/config.ts
+++ worklenz-backend/src/bin/config.ts
@@ -1,6 +1,3 @@
 import dotenv from "dotenv";
-import SegfaultHandler from "segfault-handler";
-
 dotenv.config();
 global.Promise = require("bluebird");
-SegfaultHandler.registerHandler("crash.log");
